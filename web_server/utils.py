322ada095a7413798655c7cddfd6cc9ad5ffb3ab
JasonHuynh <jasonhuynh73@berkeley.edu>
2019-05-04 11:14:17 -0700

    web server SQL queries

--- web_server/utils.py
+++ web_server/utils.py
@@ -14,17 +14,30 @@ TODO
 Obtains the domain name from a url, excluding the protocol.
 """
 def get_domains(url_list):
-    domains = []
+    """domains = []
     for url in url_list:
-        if (url != None):
-            parsed = urlparse(url).netloc
-            toks = parsed.split('.')
+        parsed = urlparse(url).netloc
+        if (parsed != None):
+            toks = parsed.split(".")
             if (len(toks) == 3):
                 toks.pop(0)
             domains.append(toks[0] + '.' + toks[1])
         else:
-            domains.append(url)
-    return domains
+            domains.append(url)"""
+    #return domains
+    return [urlparse(url).netloc for url in url_list]
+
+"""
+Strips www. from either if it exists and compares urls.
+"""
+def match_urls(a, b):
+    a_toks = a.split('.')
+    b_toks = b.split('.')
+    if (len(a_toks) == 3):
+        a_toks.pop(0)
+    if (len(b_toks) == 3):
+        b_toks.pop(0)
+    return a_toks == b_toks
 
 """
 TODO
@@ -38,4 +51,4 @@ def sentiment_negative(tweet):
 Produces a manipulation score for a tweet using sentiment analysis.
 """
 def sentiment_manipulation(tweet):
-    return 0
+    return 1
